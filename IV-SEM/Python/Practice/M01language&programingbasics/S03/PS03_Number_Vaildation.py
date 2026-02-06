d62712f3d1ef52122c25bfd28b0d554d58c8941c
pranav1334 <pranavmyvs133@gmail.com>
2026-02-06 10:02:39 +0530

    mes1

---
+++ IV-SEM/Python/Practice/M01language&programingbasics/S03/PS03_Number_Vaildation.py
@@ -0,0 +1,18 @@
+# n=int(input("enter n:"))
+# fact= 1
+# for i in range(1,n+1):
+#     fact *=i
+# print(fact)    
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not a prime number")
+else:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
+
