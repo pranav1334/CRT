495832d1d13c2d13d5edbf69dc14548936bf2b64
pranav1334 <pranavmyvs133@gmail.com>
2026-02-11 16:43:42 +0530

    mess

--- IV-SEM/Python/Practice/M01language&programingbasics/PS05_Basic_Patterns.py
+++ IV-SEM/Python/Practice/M01language&programingbasics/PS05_Basic_Patterns.py
@@ -17,11 +17,12 @@
 #         print("*",end=" ")
 #     print()
 
-# n = int(input("enter number:"))
-# for i in range(n):
-#     for j in range(n):
-#         if i == 0 or i == n-1 or j == 0 or j == n-1:
-#             print("*", end=" ")
-#         else:
-#             print(" ", end=" ")
-#     print()
+n = int(input("enter number:"))
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n-1 or j == 0 or j == n-1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+    print()
