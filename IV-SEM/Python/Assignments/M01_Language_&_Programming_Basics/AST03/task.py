321ff5183f71630650ea328bf8d63f50bc6eff97
pranav1334 <pranavmyvs133@gmail.com>
2026-02-25 15:25:38 +0530

    change

--- IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
+++ IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
@@ -1,15 +1,14 @@
 def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
-    average = (n1 + n2 + n3) / 3
-    
-    if average >= 40:   # Assuming pass mark is 40
-        status = "Pass"
-    else:
-        status = "Fail"
-    
-    return f"Average grade: {average:.2f}, Status: {status}"
+    avg = (n1 + n2 + n3) / 3
+    avg = int(avg * 100) / 100
+
+    status = "Pass" if avg >= 50 else "fail"
+    return f"Average grade: {avg}, Status: {status}"
 
 
 if __name__ == '__main__':
     name = input()
-    n1, n2, n3 = list(map(int, input().split()))
+    n1, n2, n3 = map(int, input().split())
     print(Student_Grade_System(name, n1, n2, n3))
+
+
--- IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST04/task.py
+++ IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST04/task.py
@@ -1,10 +1,11 @@
-def reverse_string(s: str) -> str:
-    reversed_str = ""
-    for char in s:
-        reversed_str = char + reversed_str
-    return reversed_str
+def Reverse_String(s: str) -> str:
+   new_str = ""
+   for i in range(len(s)-1, -1, -1):
+       new_str += s[i]
+   return new_str 
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     s = input()
-    print(reverse_string(s)) 
+    print(Reverse_String(s))
