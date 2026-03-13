9729f2e5b0bcf344c78d206fb0b9c53160490c1c
pranav1334 <pranavmyvs133@gmail.com>
2026-03-13 10:39:14 +0530

    new

---
+++ IV-SEM/Python/Practice/M04_Conceptual_Foundations/S02/PS02_Arrays.py
@@ -0,0 +1,16 @@
+# a = [10,23,45,65,1,2,3]
+# a.sort()
+# print(a[::-1])
+
+# a = [10,23,45,65,1,2,3]
+# a.sort()
+# a.reverse()
+# print(a)
+
+# a = [10,23,45,65,1,2,3]
+# rev=[]
+# for i in range(len(a)):
+#     rev.append(a[len(a)-1-i])
+# print(rev)    
+
+    
